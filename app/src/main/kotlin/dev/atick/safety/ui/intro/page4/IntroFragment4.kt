f34541ab5255586278c588bccf513fb0b714f9a0
Atick Faisal <atickfaisal@gmail.com>
2023-03-04 01:09:57 +0300

    Implement intro page 4

--- app/src/main/kotlin/dev/atick/safety/ui/intro/page4/IntroFragment4.kt
+++ app/src/main/kotlin/dev/atick/safety/ui/intro/page4/IntroFragment4.kt
@@ -1,17 +1,18 @@
-package dev.atick.safety.ui.intro.page3
+package dev.atick.safety.ui.intro.page4
 
 import androidx.compose.runtime.Composable
 import dagger.hilt.android.AndroidEntryPoint
 import dev.atick.core.ui.base.BaseFragment
 import dev.atick.core.ui.theme.JetpackTheme
+import dev.atick.safety.ui.intro.page3.IntroScreen3
 
 @AndroidEntryPoint
-class IntroFragment3 : BaseFragment() {
+class IntroFragment4 : BaseFragment() {
 
     @Composable
     override fun ComposeUi() {
         JetpackTheme {
-            IntroScreen3(onNextClick = { /*TODO*/ })
+            IntroScreen4(onNextClick = { /*TODO*/ })
         }
     }
 
