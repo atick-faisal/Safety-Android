0b83bc2c5d826f5032a911ddf2101014d3c391bb
Atick Faisal <atickfaisal@gmail.com>
2023-03-05 21:21:46 +0300

    Implement home ui

--- app/src/main/kotlin/dev/atick/safety/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/dev/atick/safety/ui/home/HomeScreen.kt
@@ -1,57 +1,182 @@
 package dev.atick.safety.ui.home
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Contacts
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.TaskAlt
+import androidx.compose.material.icons.filled.Watch
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.stringResource
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.atick.safety.R
-import dev.atick.safety.ui.home.components.HomeContent
-import dev.atick.core.ui.components.TopBar
-import kotlinx.coroutines.launch
 
-@Composable
+
 @OptIn(ExperimentalMaterial3Api::class)
-fun HomeScreen(
-    homeViewModel: HomeViewModel = viewModel()
-) {
-    val homeUiState by homeViewModel.homeUiState.collectAsState()
-    val snackbarHost = remember { SnackbarHostState() }
-    val lifecycle = LocalLifecycleOwner.current.lifecycle
-
-    homeUiState.toastMessage?.let {
-        val errorMessage = it.asString()
-        LaunchedEffect(homeUiState) {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    snackbarHost.showSnackbar(errorMessage)
-                    homeViewModel.clearError()
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        bottomBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+                    .shadow(8.dp)
+                    .background(Color.White),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Home, contentDescription = "home")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Outlined.Notifications,
+                        contentDescription = "notification"
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Contacts, contentDescription = "contacts")
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Watch, contentDescription = "device")
                 }
             }
         }
-    }
-
-    Scaffold(
-        topBar = {
-            TopBar(
-                title = stringResource(R.string.home),
-                onSearchClick = {},
-                onMenuClick = {})
-        },
-        snackbarHost = { SnackbarHost(hostState = snackbarHost) }
     ) { paddingValues ->
-        HomeContent(
+        Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-            item = homeUiState.item,
-            loading = homeUiState.loading,
-            onButtonCLick = { homeViewModel.getItem() }
-        )
+                .padding(paddingValues)
+                .background(Color.White)
+                .padding(32.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(160.dp)
+                    .align(Alignment.CenterHorizontally),
+                painter = painterResource(id = R.drawable.app_icon),
+                contentDescription = "icon"
+            )
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Safety App",
+                fontSize = 48.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Row(
+                modifier = Modifier.height(IntrinsicSize.Max),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .weight(1F)
+                        .fillMaxHeight(),
+                    color = Color(0xFFFF7D7D),
+                    shape = RoundedCornerShape(16.dp),
+                    onClick = {}
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "02", fontSize = 56.sp, color = Color.White)
+                        Text(
+                            text = "Your week’s total falls",
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+
+                Surface(
+                    modifier = Modifier
+                        .weight(1F)
+                        .fillMaxHeight(),
+                    color = Color(0xFF54C1FF),
+                    shape = RoundedCornerShape(16.dp),
+                    onClick = {}
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.TaskAlt,
+                            contentDescription = "connection",
+                            Modifier
+                                .size(64.dp)
+                                .weight(1F),
+                            tint = Color.White
+                        )
+                        Text(
+                            text = "Safety bracelet Connected",
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            }
+
+            Surface(shape = RoundedCornerShape(16.dp), color = Color(0xFFE373FF), onClick = {}) {
+                Text(
+                    text = "Alarm",
+                    fontSize = 32.sp,
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
+
+            Text(text = "Recent Falls", fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFFCECECE))
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "Brother Nawaf", fontSize = 18.sp, color = Color(0xFF4E4E4E))
+                    Column(
+                        horizontalAlignment = Alignment.End
+                    ) {
+                        Text(text = "25-02-1023", color = Color(0xFF4E4E4E))
+                        Text(text = "14:34", color = Color(0xFF4E4E4E))
+                    }
+                }
+            }
+
+            Text(
+                text = "See All",
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { }
+            )
+        }
     }
 }
