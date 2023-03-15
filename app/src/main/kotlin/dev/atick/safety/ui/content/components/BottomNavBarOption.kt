dd9c3cf9318ddab6deb877b62321eeee86a04a10
Atick Faisal <atickfaisal@gmail.com>
2023-03-16 01:10:21 +0300

    Refactor

--- app/src/main/kotlin/dev/atick/safety/ui/content/components/BottomNavBarOption.kt
+++ app/src/main/kotlin/dev/atick/safety/ui/content/components/BottomNavBarOption.kt
@@ -1,17 +1,24 @@
 package dev.atick.safety.ui.content.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Watch
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import dev.atick.safety.R
 import dev.atick.safety.ui.content.state.ScreenName
 
 @Composable
@@ -35,13 +42,18 @@ fun BottomNavBarOption(
             )
         }
         Icon(
-            imageVector = Icons.Outlined.Notifications,
-            contentDescription = "notification"
+            imageVector = when (screenName) {
+                ScreenName.Home -> Icons.Outlined.Home
+                ScreenName.Notifications -> Icons.Outlined.Notifications
+                ScreenName.Contacts -> Icons.Outlined.Person
+                ScreenName.Devices -> Icons.Outlined.Watch
+            },
+            contentDescription = "$screenName"
         )
         if (showNotificationDot) {
             Icon(
                 imageVector = Icons.Default.Circle,
-                contentDescription = "dot",
+                contentDescription = stringResource(R.string.notification_dot),
                 modifier = Modifier
                     .padding(bottom = 16.dp, start = 16.dp)
                     .size(8.dp),
--- app/src/main/kotlin/dev/atick/safety/ui/content/contacts/ContactsScreen.kt
+++ app/src/main/kotlin/dev/atick/safety/ui/content/contacts/ContactsScreen.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.atick.safety.R
 import dev.atick.safety.data.contacts.Contact
 import dev.atick.safety.ui.content.contacts.components.ContactCard
 
@@ -23,7 +25,7 @@ fun ContactsScreen(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text(text = "Contacts", fontSize = 24.sp)
+        Text(text = stringResource(R.string.contacts), fontSize = 24.sp)
         LazyColumn(
             modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(16.dp)
