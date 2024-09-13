4162c727b5208f36c0871c07347eb5acbe75aa6c
Atick <atickfaisal@gml.o>
2024-09-13 21:26:26 +0300

    Fix deprecated APIs

--- app/src/main/kotlin/dev/atick/safety/ui/auth/login/LoginScreen.kt
+++ app/src/main/kotlin/dev/atick/safety/ui/auth/login/LoginScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
--- app/src/main/kotlin/dev/atick/safety/ui/intro/page5/IntroScreen5.kt
+++ app/src/main/kotlin/dev/atick/safety/ui/intro/page5/IntroScreen5.kt
@@ -4,9 +4,17 @@ package dev.atick.safety.ui.intro.page5
 import android.app.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -99,7 +107,7 @@ fun IntroScreen5(
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 IconButton(onClick = onBackClick) {
-                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back")
+                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "back")
                 }
                 LargeButton(
                     text = "Launch App",
