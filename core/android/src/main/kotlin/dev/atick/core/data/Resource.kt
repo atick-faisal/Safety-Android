a3a523e30d50a01cd6ad6bfc1a866f2dff2129f6
Atick Faisal <atickfaisal@gmail.com>
2024-12-14 18:04:19 +0300

    Remove network bound resource

--- core/android/src/main/kotlin/dev/atick/core/data/Resource.kt
+++ core/android/src/main/kotlin/dev/atick/core/data/Resource.kt
@@ -10,26 +10,3 @@ sealed class Resource<T>(
     class Loading<T>(data: T? = null) : Resource<T>(data)
     class Error<T>(data: T? = null, error: Throwable) : Resource<T>(data, error)
 }
-
-inline fun <ResultType, RequestType> networkBoundResource(
-    crossinline query: () -> Flow<ResultType>,
-    crossinline fetch: suspend () -> RequestType,
-    crossinline saveFetchedResult: suspend (RequestType) -> Unit,
-    crossinline shouldFetch: (ResultType) -> Boolean = { true }
-) = flow {
-    val data = query().first()
-
-    val flow = if (shouldFetch(data)) {
-        emit(Resource.Loading(data))
-        try {
-            saveFetchedResult(fetch())
-            query().map { Resource.Success(it) }
-        } catch (throwable: Throwable) {
-            query().map { Resource.Error(it, throwable) }
-        }
-    } else {
-        query().map { Resource.Success(it) }
-    }
-
-    emitAll(flow)
-}
